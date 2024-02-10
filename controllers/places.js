1e9fdf2bde0c92bf4c0f50b9aa6beb2d7a89c5f5
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-02-10 15:23:26 -0500

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,14 +1,26 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+// More code ...
+
+
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
-
-
-// GET /places
-app.get('/', (req, res) => {
-    res.render('places/index')
-  })
-
 module.exports = router 
