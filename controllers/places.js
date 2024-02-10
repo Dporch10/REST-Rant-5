f3b4900dc5288cd44d7919560d9e7bb933f6bf09
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-02-10 14:15:29 -0500

    rant 5

---
+++ controllers/places.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('GET /places')
+})
+
+module.exports = router
+
+// GET /places
+app.get('/', (req, res) => {
+    res.render('places/index')
+  })
---
+++ views/home.jsx
@@ -0,0 +1,9 @@
+function home () {
+    return (
+      <Def>
+          <main>
+              <h1>HOME</h1>
+          </main>
+      </Def>
+    )
+  }
